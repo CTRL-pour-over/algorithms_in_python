15add81c03ad4d6f3bdab45dfb0744a691340cf0
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2020-01-07 21:50:12 -0500

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,26 @@
 # Binary Search Algorithm #
 # Goal: Within a given list; Return index position of arg_value. If not found, return -1. #
 
-ordered_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 22, 53]
+ordered_list = list(range(-984189, 9989859))
 
 def binary_search(arg_value, arg_ordered_list): 
-	''' Return index position of arg_value within sorted list using binary search algorithm. If arg_value not found, return -1.
-		arg_value = int to be found within the list
-		first_pos = first position in range
-		last_pos = last position in range
+	''' Return index position of arg_value within sorted list using binary search algorithm. If arg_value not found, return -1.\n
+		arg_value = int being scanned for\n
+		first_pos = first position in rang\n
+		last_pos = last position in range\n
 		mid_pos = mid position in range '''  
 	
-	first_pos = 0 
-  
-	last_pos = arg_ordered_list.__len__() - 1 
-
+	first_pos = 0 # was the item at location zero: list[0]
+   
+	last_pos = arg_ordered_list.__len__() - 1
+ 
 	while first_pos <= last_pos:    
-		mid_point = first_pos + last_pos // 2 # Find the mid point.
+		mid_point = (first_pos + last_pos) // 2 # added parentheses to equation. Oops
 		
 		print('\n', 'List: ', arg_ordered_list, '\n', 'Length of List: ', arg_ordered_list.__len__(), '\n', 'Looking for int: ', arg_value, '\n', 'First Position: ', first_pos, '\n', 'Last Position: ', last_pos, '\n', 'Mid Point: ', mid_point)
 		
 		if arg_ordered_list[mid_point] == arg_value:
-			return arg_value
+			return mid_point # was arg_value
 
 		elif arg_ordered_list[mid_point] > arg_value:
 			last_pos = mid_point - 1
@@ -30,4 +30,7 @@ def binary_search(arg_value, arg_ordered_list):
 	print('Failed to locate int')
 	return -1
 
-binary_search(11, ordered_list)
+def _run():
+    print('Item in index location: ', binary_search(42069, ordered_list))
+
+_run()
