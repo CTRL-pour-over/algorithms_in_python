616c3058d5bdd3d9d3556bbf29552a2185558daa
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2020-01-09 14:44:58 -0500

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,36 +1,22 @@
 # Binary Search Algorithm #
 # Goal: Within a given list; Return index position of arg_value. If not found, return -1. #
+a = list(range(-928293,9992345)) 
 
-ordered_list = list(range(-543,2345))
+def binary_search(arg_value, arg_ordered_list):  
+	first_pos = 0 
+	last_pos = len(a) - 1
 
-def binary_search(arg_value, arg_ordered_list): 
-	''' Return index position of arg_value within sorted list using binary search algorithm. If arg_value not found, return -1.\n
-		arg_value = int being scanned for\n
-		first_pos = first position in rang\n
-		last_pos = last position in range\n
-		mid_pos = mid position in range '''  
-	
-	first_pos = 0 # was the item at location zero: list[0]
-   
-	last_pos = arg_ordered_list.__len__() - 1
- 
-	while first_pos <= last_pos:    
-		mid_point = (first_pos + last_pos) // 2 # added parentheses to equation. Oops
-		
-		print('\n', 'List: ', arg_ordered_list[first_pos : last_pos], '\n', 'Length of List: ', arg_ordered_list.__len__(), '\n', 'Looking for int: ', arg_value, '\n', 'First Position: ', first_pos, '\n', 'Last Position: ', last_pos, '\n', 'Mid Point: ', mid_point)
-		
-		if arg_ordered_list[mid_point] == arg_value:
-			return mid_point # was arg_value
-
-		elif arg_ordered_list[mid_point] > arg_value:
-			last_pos = mid_point - 1
-
+	while first_pos <= last_pos: 
+		mid = (first_pos + last_pos) // 2		
+		if a[mid] == arg_value:
+			return mid
+		elif a[mid] > arg_value:
+			last_pos = mid - 1
 		else:
-	  		first_pos = mid_point + 1
-	print('Failed to locate int')
-	return -1
+	  		first_pos = mid + 1
+	return False
 
-def _run():
-    print('Item in index location: ', binary_search(475, ordered_list))
+def _run(arg_value):
+    print('Item in index location: ', binary_search(arg_value, a))
 
-_run()
+_run(555)
